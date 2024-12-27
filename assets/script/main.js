956b5a0a77dcd0089f28cb3b364e413c6279c607
MD Mahbub Hasan Saad <137253486+Oboshor@users.noreply.github.com>
2024-12-27 18:00:39 +0600

    Update someone function and logic

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,10 +11,11 @@ window.addEventListener("DOMContentLoaded", function () {
   calgroup.style.display = "none";
   var params = GetURLParams();
   if (Object.keys(params).length > 0 && params.x != "") {
-    xelem.value = params.x;
+    xelem.value = `${params.x}`;
     sel1elem.value = params.sel1;
     sel2elem.value = params.sel2;
     onconvert();
+    inputType();
   }
   toggleConvertButton();
 });
@@ -88,18 +89,35 @@ function decimal2base(dec, y, b) {
   if (nd > 6) nd = 6;
   if (id >= 0 && nd > 0) {
     txt =
-      "Multiply the number with the destination base raised to the power of decimals of the result (up to 6 digits resolution):<br>";
-    txt += "floor(" + dec + "&times;" + b + "<sup>" + nd + "</sup>) = ";
+      "Multiply the number with the destination base raised to the power of decimals of the result (up to 6 digits resolution) :";
+    txt +=
+      "<div class='mt-2'> floor(" +
+      dec +
+      "&times;" +
+      b +
+      "<sup>" +
+      nd +
+      "</sup>) = ";
     dec = Math.floor(dec * Math.pow(b, nd));
-    txt += dec;
+    txt += dec + "</div>";
   }
   document.getElementById("cal2dec").innerHTML = txt;
   for (let n = dec, k = 0; n >= 1; k++) {
     row = "<tr>";
-    row += "<td>" + n + "/" + b + "</td>";
-    row += "<td class='TDs'>" + Math.floor(n / b) + "</td>";
-    row += "<td class='TDs'>" + (n % b).toFixed() + "</td>";
-    row += "<td class='TDs'>" + k + "</td>";
+    row +=
+      "<td class='border-gray-400 border-t px-3 py-2'>" + n + "/" + b + "</td>";
+    row +=
+      "<td class='border-gray-400 border-t px-1 py-2 text-center'>" +
+      Math.floor(n / b) +
+      "</td>";
+    row +=
+      "<td class='border-gray-600 border py-2 px-1 border-dashed bg-slate-200 text-center'>" +
+      (n % b).toFixed() +
+      "</td>";
+    row +=
+      "<td class='border-gray-400 border-t py-2 px-1 text-center'>" +
+      k +
+      "</td>";
     row += "</tr>";
     n = Math.floor(n / b);
     //$("#cal2tbl tbody").append(row);
@@ -112,20 +130,26 @@ function decimal2base(dec, y, b) {
   document.getElementById("cal2result").innerHTML =
     "= (" + y + ")<sub>" + b + "</sub>";
 }
-
-function validateInput() {
+async function validateInput() {
+  const value = xelem.value;
   const base = parseInt(sel1elem.value, 10);
-  const value = xelem.value.toUpperCase();
-  const validChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".slice(0, base) + ".";
-  const isValid = [...value].every(char => validChars.includes(char) && (char !== '.' || value.indexOf('.') === value.lastIndexOf('.')));
+  const validChars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".slice(0, base) + ".";
+  const isValid = [...value].every(
+    (char) =>
+      validChars.includes(char) &&
+      (char !== "." || value.indexOf(".") === value.lastIndexOf("."))
+  );
 
   if (!isValid) {
     xelem.classList.add("input-error");
-    const validCharsArray = validChars.slice(0, -1).split('');
+    const validCharsArray = validChars.slice(0, -1).split("");
     const lastChar = validCharsArray.pop();
-    const validCharsString = validCharsArray.join(', ') + " & " + lastChar;
+    const validCharsString = validCharsArray.join(", ") + " & " + lastChar;
     document.getElementById("inputError").style.display = "block";
-    document.getElementById("inputError").innerText = `Invalid input for base ${base}. Please use only characters: ${validCharsString}`;
+    document.getElementById(
+      "inputError"
+    ).innerText = `Invalid input for base ${base}. Please use only characters: ${validCharsString}`;
     convertBtn.disabled = true;
     return false;
   } else {
@@ -137,10 +161,10 @@ function validateInput() {
 
 function updateURLParams() {
   const params = new URLSearchParams(window.location.search);
-  params.set('x', xelem.value.toUpperCase().trim());
-  params.set('sel1', sel1elem.value);
-  params.set('sel2', sel2elem.value);
-  window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
+  params.set("x", encodeURIComponent(xelem.value));
+  params.set("sel1", sel1elem.value);
+  params.set("sel2", sel2elem.value);
+  window.history.replaceState({}, "", `${window.location.pathname}?${params}`);
 }
 
 function onconvert() {
@@ -149,25 +173,25 @@ function onconvert() {
     return;
   }
   updateURLParams();
-  var x = xelem.value.toUpperCase().trim();
+  var x = xelem.value;
   var b1 = sel1elem.selectedIndex + 2;
   var b2 = sel2elem.selectedIndex + 2;
   try {
     var y = new BigNumber(x, b1);
-    xelem.style.background = "var(--form-bg)"; // Use CSS variable for background color
+    xelem.classList.add("bg-current"); // Use CSS variable for background color
   } catch (err) {
-    xelem.style.background = "var(--input-error-bg)"; // Use CSS variable for error background color
+    xelem.classList.add("bg-red-50"); // Use CSS variable for error background color
     yelem.value = "";
     return;
   }
   var dec = y.toString(10);
-  y = y.toString(b2).toUpperCase();
+  y = y.toString(b2).toUpperCase().trim();
   yelem.value = y;
 
   var yd = yelem.value.match(/[\dA-Z]/g);
   var ylabel = "Result number";
   if (yd != null)
-    ylabel += " (" + yd.length + (yd.length == 1) ? " digit)" : " digits)";
+    ylabel += ` ( ${yd.length} ${yd.length == 1 ? "digit )" : "digits )"}`;
   document.getElementById("ylabel").innerHTML = ylabel;
   document.getElementById("b1txt").innerHTML = b1;
   document.getElementById("b2txt").innerHTML = b2;
@@ -190,16 +214,19 @@ function onrst() {
 var inputTypeSelect = document.getElementById("sel1");
 var existingInput = document.getElementById("x");
 
-inputTypeSelect.addEventListener("change", () => {
+function inputType() {
   var selectedValue = parseInt(inputTypeSelect.value, 10);
   if (selectedValue >= 2 && selectedValue <= 10) {
     existingInput.type = "number";
   } else {
     existingInput.type = "text";
   }
-});
+}
+
+inputTypeSelect.addEventListener("change", inputType);
 
 xelem.addEventListener("input", () => {
+  xelem.value = xelem.value.toUpperCase().trim();
   validateInput();
   toggleConvertButton();
 });
